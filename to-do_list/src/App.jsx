a08bf6b2162e9b8f6acb7cf29bbb37f8fb325aba
frankMarcelino <frank.silva.ti@gmail.com>
2024-09-01 07:54:47 -0300

    feet: iniciei a implmentação da adicão de tarefas

--- to-do_list/src/App.jsx
+++ to-do_list/src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import ItemsContainer from "./ItemsContainer";
 
 import "./App.css";
 
 function App() {
+  const [taskList, setTaskList] = useState(["item 1", "item 2", "item 3"]);
   return (
     <div className="App">
-      <h1>To-Do List</h1>
-      <ItemsContainer taskList={["item 1", "item 2", "item 3"]} />
+      <h1>Lista de Tarefas</h1>
+      <h2>Adição de tarefas</h2>
+      <ItemsContainer taskList={taskList} />
     </div>
   );
 }
